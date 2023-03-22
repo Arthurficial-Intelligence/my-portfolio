49992700f8cc40036839b92bfbfec97887a9c3ad
arthur smith <arthurrankin1992@gmail.com>
2023-03-21 21:09:06 -0500

    Update under-construction.js

--- src/pages/under-construction.js
+++ src/pages/under-construction.js
@@ -19,8 +19,11 @@ const UnderConstruction = () => (
       </ul>
       <h2>About Me</h2>
       <p>
-        [Your short bio goes here. Introduce yourself, mention your education,
-        and share your interests in computer science.]
+        My name is Arthur Smith, and I am a software engineer and aspiring educator based in Nashville, Tennessee. With five years of experience in the field, I have already graduated from Nashville Software School's coding boot camp and am currently pursuing a bachelor's degree in computer science from the University of the People.
+
+        Although I am in the early stages of his academic journey, I am deeply passionate about computing in education. My interests lie in exploring the potential of technology to enhance learning experiences, promote computer science foundations, and enable personalized learning through artificial intelligence.
+
+        As a Nashville native, I am proud to contribute to the growth and development of the local tech community. My long-term career goals include becoming an educator, researcher, or advocate for the role of computing in educational settings.
       </p>
       <Link to="/">Go back to the homepage</Link>
     </div>
