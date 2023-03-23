06e1e46e2aecaec56d433b6d6c13e6189b06f342
arthur smith <arthurrankin1992@gmail.com>
2023-03-22 22:09:05 -0500

    big update

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -44,7 +45,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon-32x32.png`, // This path is relative to the root of the site.
       },
     },
   ],
--- src/components/layout.js
+++ src/components/layout.js
@@ -6,6 +6,8 @@
  */
 
 import * as React from "react"
+import { Helmet } from "react-helmet"
+
 
 import Header from "./header"
 import "./layout.css"
@@ -14,6 +16,10 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <Helmet>
+        <title>Arthur Smith | Researcher & Software Engineer</title>
+        <meta name="description" content="Explore the intersection of academia and software engineering with Arthur Smith. Discover insightful articles, in-depth research, and a professional portfolio showcasing a unique blend of knowledge, innovation, and experience." />
+      </Helmet>
       <Header siteTitle={"Arthurficial Intelligence"} />
       <div
         style={{
